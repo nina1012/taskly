db0059fd464b3a5a8dd908e89c49bee344960eda
Nina <labferment10@gmail.com>
2025-02-02 13:56:33 +0100

    stack, modal and tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping list" }} />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="counter" />
+      {/* <Stack.Screen
+        name="counter"
+        options={{ title: "Counter", presentation: "modal" }}
+      /> */}
+      <Tabs.Screen name="idea" options={{ title: "Idea" }} />
+    </Tabs>
   );
 }
--- app/counter.tsx
+++ app/counter.tsx
@@ -1,16 +1,16 @@
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function CounterScreen() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.navigate("/idea")}>
+        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
+          Go to /idea
+        </Text>
+      </TouchableOpacity>
       <Text style={styles.text}>Counter</Text>
-      <Link
-        href="/"
-        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
-      >
-        Go to /
-      </Link>
     </View>
   );
 }
