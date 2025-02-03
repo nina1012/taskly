1513d076a6d6ad4c296bb6efe327ca7036625e6c
Nina <labferment10@gmail.com>
2025-02-03 21:44:40 +0100

    added reminder

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,42 +1,98 @@
 import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
-import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+import { intervalToDuration, isBefore } from "date-fns";
+import { TimeSegment } from "../../components/TimeSegment";
+// 10 seconds from now
+const timestamp = Date.now() + 10 * 1000;
+type CountdownStatus = {
+  isOverdue: boolean;
+  distance: ReturnType<typeof intervalToDuration>;
+};
 
 export default function CounterScreen() {
+  const [status, setStatus] = useState<CountdownStatus>({
+    isOverdue: false,
+    distance: {},
+  });
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const isOverdue = isBefore(timestamp, Date.now());
+      const distance = intervalToDuration(
+        isOverdue
+          ? { end: Date.now(), start: timestamp }
+          : // eslint-disable-next-line prettier/prettier
+            { start: Date.now(), end: timestamp }
+      );
+      setStatus({ isOverdue, distance });
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
-    console.log(result);
     if (result === "granted") {
-      console.log(Notifications);
-      // await Notifications.scheduleNotificationAsync({
-      //   content: {
-      //     title: "I'm a notification from your app 📨",
-      //   },
-      //   trigger: {
-      //     seconds: 5,
-      //     type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-      //     repeats: false,
-      //   },
-      // });
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app! 📨",
+        },
+        trigger: {
+          seconds: 5,
+        } as Notifications.TimeIntervalTriggerInput,
+      });
     } else {
-      if (Device.isDevice)
-        Alert.alert(
-          "Unable to schedule notification",
-          // eslint-disable-next-line prettier/prettier
-          "Enable the notification permission for Expo Go in settings"
-        );
+      Alert.alert(
+        "Unable to schedule notification",
+        // eslint-disable-next-line prettier/prettier
+        "Enable the notifications permission for Expo Go in settings"
+      );
     }
   };
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        status.isOverdue ? styles.containerLate : undefined,
+      ]}
+    >
+      {!status.isOverdue ? (
+        <Text style={[styles.heading]}>Thing due in</Text>
+      ) : (
+        <Text style={[styles.heading, styles.whiteText]}>Thing overdue by</Text>
+      )}
+
+      <View style={styles.row}>
+        status.isOverdue ? styles.containerLate : undefined,
+        <TimeSegment
+          unit="Days"
+          number={status.distance.days ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={status.distance.hours ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+      </View>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
         onPress={scheduleNotification}
       >
-        <Text style={styles.buttonText}>Schedule notification</Text>
+        <Text style={styles.buttonText}>I've done the thing!</Text>
       </TouchableOpacity>
     </View>
   );
@@ -59,5 +115,21 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textTransform: "uppercase",
     letterSpacing: 1,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 24,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+    color: theme.colorBlack,
+  },
+  containerLate: {
+    backgroundColor: theme.colorRed,
+  },
+  whiteText: {
+    color: theme.colorWhite,
   },
 });
